da86c42165b51433a3822a332e7fe5c56ec0a217
Jesse <rank_booster@yahoo.com>
2018-07-06 10:38:31 -0600

    updated controller

--- public/server/server.js
+++ public/server/server.js
@@ -1,21 +1,29 @@
 
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const bc = require('./controller/quotes_controller.js');
+const quoContr = require('./controllers/quotes_controller.js');
 let app = express();
-const port = 3006;
+// const port = 3006;  // not needed, put port value in .env
 
 
 app.use(bodyParser.json());
-app.get('/api/quotes', bc.read);
-app.post('/api/quotes', bc.create);
-app.put('/api/quotes/:id', bc.update);
-app.delete('/api/quotes/:id', bc.delete);
-
+app.get('/api/quotes', quoContr.read);
+app.post('/api/quotes', quoContr.create);
+app.put('/api/quotes/:id', quoContr.update);
+app.delete('/api/quotes/:id', quoContr.delete);
 
 // app.use(express.static(__dirname + '/../build'))
 
-app.listen(port, () => {
-    console.log("Port "+port+" is open.") 
+app.listen(process.env.SERVER_PORT, () => {
+    console.log("Port "+process.env.SERVER_PORT+" is open.") 
 })
+
+// app.listen(process.env.SERVER_PORT, () => {
+//     console.log("Port "+process.env.SERVER_PORT+" is open.") 
+// })
+
+// app.listen(port, () => {
+//     console.log("Port "+port+" is open.") 
+// })
 
