f31887f2ec50d416fa6caa415d8d017d4f30aad5
Jesse <rank_booster@yahoo.com>
2018-07-06 21:40:38 -0600

    It's alive!!

--- src/ShowList.js
+++ src/ShowList.js
@@ -10,11 +10,13 @@ class ShowList extends Component {
         this.state = {
             quoteList: [],
             editedQuote: "",
-            editable: false
+            editable: false,
+            selectedQuote: ""
         }
         this.handleInput = this.handleInput.bind(this);
         this.saveInput = this.saveInput.bind(this);
         this.handleClick = this.handleClick.bind(this)
+        this.selectQuote = this.selectQuote.bind(this)
  
     }
 
@@ -32,17 +34,40 @@ class ShowList extends Component {
     }
 
       saveInput(){
-        let promise = axios.put('http://localhost:3006/api/quotes/0', {
+        let promise = axios.put('http://localhost:3006/api/quotes/'+this.state.selectedQuote.id, {
             text: this.state.editedQuote
         })
-        promise.then(res => {   //correct
+        promise.then(res => {   
+            let quote = res.data.find(quote => {
+                if(quote.id === this.state.selectedQuote.id){
+                    return quote
+                }
+            })
+            console.log(quote);
             this.setState({   
                 quoteList: res.data,
+                selectedQuote: quote,
                 editable: false
               })           
         }) 
         } 
- 
+        // trashQuote(){
+        //     let promise = axios.delete('http://localhost:3006/api/quotes/'+id, {
+        //         text: this.state.editedQuote
+        //     })
+        //     promise.then(res => {  
+        //         this.setState({   
+        //             quoteList: res.data,
+        //           })           
+        //     }) 
+        //     } 
+
+     selectQuote(value){
+        this.setState({   
+            selectedQuote: value
+          })
+     }
+
       componentDidMount(){ 
         let promise = axios.get('http://localhost:3006/api/quotes')
         promise.then(res => {   
@@ -53,12 +78,13 @@ class ShowList extends Component {
         } 
  
     render(){ 
-        let quoteArray = this.state.quoteList.map(function(quo,i){
+        const quoteArray = this.state.quoteList.map((quo,i) => {
             return (
-                <span className="item" key={i}>{quo.author} on {quo.category}<br /></span>
+                <div className="item" key={i} onClick={() => this.selectQuote(quo)} > {quo.author} on {quo.category}<br /></div>
             )
         });
 
+        var selectedQuote = this.state.selectedQuote || this.state.quoteList[0];
 
         return (
         <span>
@@ -70,7 +96,7 @@ class ShowList extends Component {
             </div>
 
             <div className="displayBox">
-            <DisplayQuote showNow = {this.state.quoteList[0]}
+            <DisplayQuote showNow = {selectedQuote}
             makeChange = {this.handleInput}
             saveChange = {this.saveInput} 
             editable = {this.state.editable} 
